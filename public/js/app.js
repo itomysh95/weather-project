6254b3fa4c9671e598ff7b197e3b385f9eb485a5
Yuanshan Huang <thomas_h@live.ca>
2020-01-03 12:30:43 -0500

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -20,7 +20,7 @@ weatherForm.addEventListener('submit',(e)=>{
     weatherMessage.textContent = " "
 
     if(location){
-        fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+        fetch('/weather?address='+location).then((response)=>{
         response.json().then((data)=>{
             if(data.error){
                 errorMessage.textContent = data.error
